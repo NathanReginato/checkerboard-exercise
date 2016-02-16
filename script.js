32d19b804ad795223fa715fd6d147dfaabf5f16c
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-15 17:04:11 -0700

    Finished random color page

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Your JS goes here
 
+function getRandomColor() {
+  var x = Math.floor(Math.random() * 255);
+  var y = Math.floor(Math.random() * 255);
+  var z = Math.floor(Math.random() * 255);
+  var color = "rgb(" + x + ", " + y + ", " + z + ")";
+  return color;
+}
+
 for (var i = 0; i < 99; i++) {
   var div = document.createElement("div");
   document.body.appendChild(div);
   div.style.float="right";
   div.style.paddingBottom="11.1%";
   div.style.width="11.1%";
-  if (i % 2 === 0) {
-    div.style.backgroundColor="red";
-  } else {
-    div.style.backgroundColor="black  ";
-  }
-  console.log(div)
+  div.style.backgroundColor=getRandomColor();
 };
